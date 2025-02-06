6e1b1803e3a9a053f0486e29806448a8f0680318
Christian Landeros <landeros@mit.edu>
2025-02-06 19:51:25 +0000

    test sagemaker preprocessing

--- src/data/test_sagemaker.py
+++ src/data/test_sagemaker.py
@@ -1,5 +1,8 @@
+"""
+test_sagemaker.py
+"""
 import sagemaker
-from sagemaker.processing import ScriptProcessor
+from sagemaker.processing import ScriptProcessor, ProcessingInput, ProcessingOutput
 from sagemaker.s3 import S3Uploader
 import boto3
 import os
@@ -16,14 +19,22 @@ logger.setLevel(logging.DEBUG)
 def main():
     # set up arg parsing
     parser = argparse.ArgumentParser(description="Data loading script")
+    parser.add_argument("--profile", type=str, default="651340551631_AWSPowerUserAccess", help="AWS profile name")
     parser.add_argument("--bucket", type=str, default="tcga-riskformer-data-2025", help="S3 bucket name")
-    parser.add_argument("--input-path", type=str, default="raw", help="Path to input data")
-    parser.add_argument("--output-path", type=str, default="processed", help="Path to output data")
+    parser.add_argument("--input_dir", type=str, default="testing_raw", help="Path to input data")
+    parser.add_argument("--output_dir", type=str, default="testing_processed", help="Path to output data")
+    parser.add_argument("--filename", type=str, default="input.txt", help="Name of file to process")
     args = parser.parse_args()
 
-    s3_client = initialize_s3_client()
+    s3_client, session = initialize_s3_client(args.profile, return_session=True)
+    s3_client.put_object(Bucket=args.bucket, Key=f"{args.input_dir}/{args.filename}", Body="Test input data")
+    logger.info(f"Created s3://{args.bucket}/{args.input_dir}/{args.filename}")
 
+    sagemaker_session = sagemaker.Session(boto_session=session)
+    logger.debug(f"Using sagemaker session: {sagemaker_session}")
 
+    role = sagemaker.get_execution_role()
+    logger.debug(f"Using role: {role}")
 
 if __name__ == "__main__":
     main()
