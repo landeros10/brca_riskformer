24a108071e7f5c13a9a3118370f207841ec4ce4c
Christian <landeros@mit.edu>
2025-02-09 22:45:36 -0500

    rearranging utils funtions

--- src/data/model_loading.py
+++ src/data/model_loading.py
@@ -9,9 +9,7 @@ import time
 import numpy as np
 
 from src.logger_config import logger_setup
-from src.data.data_utils import (initialize_s3_client,
-                                 upload_large_files_to_bucket, wipe_bucket)
-from src.utils import set_seed
+from src.utils import set_seed, initialize_s3_client, upload_large_files_to_bucket, wipe_bucket
 logger = logging.getLogger(__name__)
 
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))  # Moves up to project root
