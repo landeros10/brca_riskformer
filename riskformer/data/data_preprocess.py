c78ef2000a78cb130783d5407c8684f45c538a3c
Christian <landeros@mit.edu>
2025-02-27 15:05:48 -0500

    readd

--- riskformer/data/data_preprocess.py
+++ riskformer/data/data_preprocess.py
@@ -218,6 +218,7 @@ def load_uni_encoder(model_path, config_files, device):
     try:
         transform = transforms.Compose(
             [
+                transforms.Resize(224),
                 transforms.ToTensor(),
                 transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
             ]
